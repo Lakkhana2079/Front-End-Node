a3d28475bc8eb1ffba027c766e877705247d8178
Lakkhana2079 <lakkhana2079@gmail.com>
2023-09-19 11:27:18 +0700

    update

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API 
 //const base_url = "https://api.example.com"; 
-const base_url = "node50268-lakkhana.proen.app.ruk-com.cloud:11495"; 
+const base_url = "https://node50268-lakkhana.proen.app.ruk-com.cloud:11495"; 
 
 // Set the template engine 
 app.set('view engine', 'ejs'); 
