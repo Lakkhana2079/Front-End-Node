7dc46a8e8fc86727dff0ea71b118bd1d62283b7b
Lakkhana2079 <lakkhana2079@gmail.com>
2023-11-01 21:54:35 +0700

    update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "http://10.104.12.114";
-//const base_url = "http://localhost:3000";
-const base_url = "http://node50268-lakkhana.proen.app.ruk-com.cloud";
+const base_url = "http://localhost:3000";
+//const base_url = "http://node50263-lakkhana.proen.app.ruk-com.cloud";
 
 // Set the template engine
 app.set('view engine' , 'ejs');
