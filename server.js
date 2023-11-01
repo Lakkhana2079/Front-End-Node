12052438e10948a8939e8af34b95c67ae1dcb4c4
Lakkhana2079 <lakkhana2079@gmail.com>
2023-11-01 18:38:47 +0700

    update

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 // Base URL for the API
-// const base_url = "https://api.example.com";
-const base_url = "http://localhost:3000";
+const base_url = "https://10.104.12.114";
+//const base_url = "http://localhost:3000";
 
 // Set the template engine
 app.set('view engine' , 'ejs');
@@ -284,10 +284,10 @@ app.get("/data/delete/:id" , async (req,res) => {
         res.status(500).send('Error');
     }
 });
-app.listen(5500, ()=> {
-    console.log('Server started on port 5500');
-});
-
-// app.listen(8080, ()=> {
-//     console.log('Server started on port 8080');
+// app.listen(5500, ()=> {
+//     console.log('Server started on port 5500');
 // });
+
+app.listen(8080, ()=> {
+    console.log('Server started on port 8080');
+});
