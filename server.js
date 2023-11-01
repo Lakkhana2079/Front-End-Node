34ed76d8dab86795aed8212d4fd48a1f9770c0ec
Lakkhana2079 <lakkhana2079@gmail.com>
2023-11-01 11:20:13 +0700

    update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 
 // Base URL for the API
 // const base_url = "https://api.example.com";
-const base_url = "http://localhost:3000";
 //const base_url = "http://localhost:3000";
+const base_url = "http://10.104.12.114";
 
 // Set the template engine
 app.set('view engine' , 'ejs');
